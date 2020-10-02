0c2f5ca88d0f64240889c8d75872413f2321dda2
Devin Bonney <faline124@yahoo.com>
2020-10-02 17:40:04 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,31 +1,22 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+// read from json file synchronously to retrieve raw data
+const budgetJSON = fs.readFileSync('./budget-data.json');
+// parse the raw data to create a readable object from the JSON
+const budget = JSON.parse(budgetJSON);
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-  ]
-};
+app.use('/', express.static('public'));
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
+    // send budget object in JSON response
     res.json(budget);
 });
 
