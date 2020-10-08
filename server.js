4c1fb0ef45eb0ade54e811c921f6a906baff1f82
Devin Bonney <faline124@yahoo.com>
2020-10-07 21:55:49 -0400

    Added CORS npm module

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
+// Budget API
+
 const express = require('express');
 const fs = require('fs');
-
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
+app.use(cors());
 // read from json file synchronously to retrieve raw data
 const budgetJSON = fs.readFileSync('./budget-data.json');
 // parse the raw data to create a readable object from the JSON
 const budget = JSON.parse(budgetJSON);
 
-app.use('/', express.static('public'));
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
